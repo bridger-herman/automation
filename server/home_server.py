7ababde746af648a5e83a04b36cadddbdabe2dc0
Bridger Herman <scooteeroo@gmail.com>
2017-11-16 17:12:33 -0600

    Server side updates for LEDs

--- server/home_server.py
+++ server/home_server.py
@@ -5,11 +5,14 @@ from led_single import SingleLED
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+led_changer = SingleLED((0, 0, 0, 0))
+
 @app.route('/', methods=['GET', 'POST'])
-def form():
+def index():
     if len(request.form) > 0:
         led_vals = [int(v) for k, v in request.form.items()]
-        SingleLED(led_vals).start()
+        led_changer.set_color(led_vals)
+        led_changer.start()
         return render_template('index.html',
                 red_value=request.form['red'],
                 green_value=request.form['green'],
