a297b2d87ce62f0269d9c59abdd7a42d337d5a37
Bridger Herman <herma582@umn.edu>
2017-11-18 08:27:24 -0600

    Slightly better interface for single-color changing

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -2,6 +2,10 @@ function init() {
   $('.color-input').on('change', function(event) {
     $(event.target).parent().parent().submit();
   })
+  let boxes = $('.color-box');
+  boxes.each(function(i) {
+    $(boxes[i]).css('background-color', $(boxes[i]).attr('data-color'));
+  });
 }
 
 document.onload = init();
