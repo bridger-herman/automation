ea2ccba26a4b5f92d6746b0b94abc14dcf769329
Bridger Herman <herma582@umn.edu>
2017-11-21 11:52:18 -0600

    Configure server to use fades

--- server/home_server.py
+++ server/home_server.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../led')
 from led_single import SingleLED
+from led_fade import LEDLinearFade
 from serial_wrapper import SerialWrapper
 
 from flask import Flask, render_template, request
@@ -9,14 +10,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 ser = SerialWrapper(sys.argv[1])
 
-led_changer = SingleLED(ser, (0, 0, 0, 0))
+current_color = [0, 0, 0, 0]
+led_obj = LEDLinearFade(ser, current_color, current_color, 1, 10)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global current_color
     if len(request.form) > 0:
         led_vals = [int(v) for k, v in request.form.items()]
-        led_changer.set_color(led_vals)
-        led_changer.start()
+        led_obj.__init__(ser, current_color, led_vals, 1, 30)
+        led_obj.start()
+        current_color = led_vals
         return render_template('index.html',
                 red_value=request.form['red'],
                 green_value=request.form['green'],
