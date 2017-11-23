20488ea2b58d963bc4e3b9373782ef7dfa50644d
Bridger Herman <herma582@umn.edu>
2017-11-22 21:27:27 -0600

    Better HTML form

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -1,12 +1,39 @@
 function init() {
+  setupInputRows();
+  // Set up direct for submit
   $('.color-input').on('change', function(event) {
     $(event.target).parent().parent().submit();
   })
+  // Set up color preview boxes
   let boxes = $('.color-preview-box');
   boxes.each(function(i) {
     $(boxes[i]).css('background-color', $(boxes[i]).attr('data-color'));
   });
+  // Set up color mixer
   setMixedColorRGB('single-led');
+}
+
+function setupInputRows() {
+  let inputRows = $('.led-input-row');
+  inputRows.each(function (i) {
+    let hex = $(inputRows[i]).attr('data-hex');
+    let name = $(inputRows[i]).attr('data-name');
+    let value = $(inputRows[i]).attr('data-value');
+    $(inputRows[i]).append(getColorPreview(hex));
+    $(inputRows[i]).append(getColorInput(name, value));
+    $(inputRows[i]).append(getValueDisplay(value));
+  });
+
+}
+
+function getColorPreview(color) {
+  return '<div class="color-preview-box" data-color="' + color + '"></div>';
+}
+function getColorInput(name, value) {
+  return '<input class="color-input" type="range" name="' + name + '" value="' + value + '" min="0" max="255">';
+}
+function getValueDisplay(value) {
+  return '<div>(' + value + ')</div>';
 }
 
 function setMixedColorRGB(inputContainer) {
