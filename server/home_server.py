f800bca360ae857312f47392b2d915efad654d82
Bridger Herman <herma582@umn.edu>
2017-11-22 20:29:08 -0600

    Make server object-oriented

--- server/home_server.py
+++ server/home_server.py
@@ -2,33 +2,42 @@ import sys
 sys.path.append('../led')
 from led_single import SingleLED
 from led_fade import LEDLinearFade
-from serial_wrapper import SerialWrapper
-
+from serial_wrapper import SerialWrapper, SerialMockup
 from flask import Flask, render_template, request
 
-# Initialize
-app = Flask(__name__)
-ser = SerialWrapper(sys.argv[1])
+class HomeServer:
+    def __init__(self, host=None):
+        self.ser = SerialMockup(db=False)
+        self.host = host
+        self.app = Flask(__name__)
+        self.current_color = [0, 0, 0, 0]
+        self.led_obj = LEDLinearFade(self.ser, self.current_color, self.current_color, 1, 10)
+        self._setup_routes()
 
-current_color = [0, 0, 0, 0]
-led_obj = LEDLinearFade(ser, current_color, current_color, 1, 10)
+    def _setup_routes(self):
+        self.app.route('/', methods=['GET', 'POST'])(self.index)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    global current_color
-    if len(request.form) > 0:
-        led_vals = [int(v) for k, v in request.form.items()]
-        led_obj.__init__(ser, current_color, led_vals, 1, 30)
-        led_obj.start()
-        current_color = led_vals
-        return render_template('index.html',
-                red_value=request.form['red'],
-                green_value=request.form['green'],
-                blue_value=request.form['blue'],
-                white_value=request.form['white']
-        )
-    else:
-        return render_template('index.html')
+    def index(self):
+        if len(request.form) > 0:
+            led_vals = [int(v) for k, v in request.form.items()]
+            self.led_obj.__init__(self.ser, self.current_color, led_vals, 1, 30)
+            self.led_obj.start()
+            self.current_color = led_vals
+            return render_template('index.html',
+                    red_value=request.form['red'],
+                    green_value=request.form['green'],
+                    blue_value=request.form['blue'],
+                    white_value=request.form['white']
+            )
+        else:
+            return render_template('index.html')
+
+    def run(self):
+        if self.host is None:
+            self.app.run()
+        else:
+            self.app.run(host=self.host)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    s = HomeServer()
+    s.run()
