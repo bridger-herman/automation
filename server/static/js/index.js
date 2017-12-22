6edbdced74987edb0f8e27ab922c81bb0db22ea8
Bridger Herman <scooteeroo@gmail.com>
2017-12-22 09:54:36 -0600

    Various resizes

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -7,7 +7,6 @@ function init() {
   })
   // Set up color mixer
   $('.wheel-color-picker').on('sliderup', function(event){
-    // console.log($(event.target).wheelColorPicker('color'));
     setSliderRGBW($(event.target).parent().parent());
     $(event.target).parent().parent().submit();
   });
@@ -22,13 +21,8 @@ function setupInputRows() {
   inputRows.each(function (i) {
     let hex = $(inputRows[i]).attr('data-hex');
     let name = $(inputRows[i]).attr('data-name');
-    // $(inputRows[i]).append(getColorPreview(hex));
     $(inputRows[i]).append(getColorInput(name, 255, hex)); // Default to #ffffffff
   });
-  // let boxes = $('.color-preview-box');
-  // boxes.each(function(i) {
-  //   $(boxes[i]).css('background-color', $(boxes[i]).attr('data-color'));
-  // });
   let ranges = $('.color-input');
   ranges.each(function(i) {
     $(ranges[i]).css('background', $(ranges[i]).attr('data-color'));
@@ -59,10 +53,6 @@ function setSliderRGBW(inputContainer) {
     $('#' + names[i]).val(rgbw[i]);
   }
   $(inputContainer).find('.color-mixer-box').css('background-color', rgbToHex(...rgbwToRGB(...rgbw)));
-}
-
-function getColorPreview(color) {
-  return '<div class="color-preview-box" data-color="' + color + '"></div>';
 }
 
 function getColorInput(name, value, color) {
