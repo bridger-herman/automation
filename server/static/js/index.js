2442f42fcaf2cd907be99dafa00ff303244ff273
Bridger Herman <herma582@umn.edu>
2018-05-13 08:55:50 -0500

    Move ajax helpers to file

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -16,41 +16,6 @@ function sendGradientUpdates() {
   ajPOST('set-gradient', data, function() {});
 }
 
-function ajGET(url, onSuccess) {
-  $.ajax({
-    url: url,
-    type: "get",
-    contentType: "application/json",
-    dataType: "json",
-    success: function(response) {
-      console.log(response);
-      onSuccess(response);
-    },
-    error: function(xhr) {
-      console.log('error');
-      console.log(xhr);
-    }
-  });
-}
-
-function ajPOST(url,data, onSuccess) {
-  $.ajax({
-    url: url,
-    type: "post",
-    contentType: "application/json",
-    dataType: "json",
-    data: JSON.stringify(data, null, '\t'), // AHHHH stringify so many hours were wasted here. Kept getting 400 error https://stackoverflow.com/a/17082422
-    success: function(response) {
-      console.log(response);
-      onSuccess(response);
-    },
-    error: function(xhr) {
-      console.log('error');
-      console.log(xhr);
-    }
-  });
-}
-
 function init() {
   ajGET('get-gradient', updateGradientControls);
   $('#gradient-loop').on('change', sendGradientUpdates);
