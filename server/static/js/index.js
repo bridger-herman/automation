500638b0d0402edd9d0a0902250d68692e47da61
Bridger Herman <herma582@umn.edu>
2018-05-12 23:13:40 -0500

    Add ajax helpers

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -4,19 +4,45 @@ function updateGradientControls(info) {
   $('#gradient-duration').attr('value', info.duration);
 }
 
+function toggleGradientPlay() {
+
+}
+
 function sendGradientUpdates() {
   let src = $('#gradient-preview').attr('src');
   let loop = $('#gradient-loop').attr('value');
   let duration = $('#gradient-duration').attr('value');
+  let data = {'src':src, 'loop':loop, 'duration':duration};
+  ajPOST('set-gradient', data, function() {});
+}
+
+function ajGET(url, onSuccess) {
   $.ajax({
-    url: "set-gradient",
+    url: url,
+    type: "get",
+    contentType: "application/json",
+    dataType: "json",
+    success: function(response) {
+      console.log(response);
+      onSuccess(response);
+    },
+    error: function(xhr) {
+      console.log('error');
+      console.log(xhr);
+    }
+  });
+}
+
+function ajPOST(url,data, onSuccess) {
+  $.ajax({
+    url: url,
     type: "post",
     contentType: "application/json",
     dataType: "json",
-    data: JSON.stringify({'src':src, 'loop':loop, 'duration':duration}, null, '\t'), // AHHHH stringify so many hours were wasted here. Kept getting 400 error https://stackoverflow.com/a/17082422
+    data: JSON.stringify(data, null, '\t'), // AHHHH stringify so many hours were wasted here. Kept getting 400 error https://stackoverflow.com/a/17082422
     success: function(response) {
       console.log(response);
-      updateGradientControls(response);
+      onSuccess(response);
     },
     error: function(xhr) {
       console.log('error');
@@ -26,50 +52,9 @@ function sendGradientUpdates() {
 }
 
 function init() {
-  $.ajax({
-    url: "get-gradient",
-    type: "get",
-    contentType: "application/json",
-    dataType: "json",
-    // data: JSON.stringify({rgbw:currentRGBW}, null, '\t'), // AHHHH stringify so many hours were wasted here. Kept getting 400 error https://stackoverflow.com/a/17082422
-    success: function(response) {
-      console.log(response);
-      updateGradientControls(response);
-    },
-    error: function(xhr) {
-      console.log('error');
-      console.log(xhr);
-    }
-  });
-  // $('#gradient-preview').on('update', sendGradientUpdates);
+  ajGET('get-gradient', updateGradientControls);
   $('#gradient-loop').on('change', sendGradientUpdates);
   $('#gradient-duration').on('change', sendGradientUpdates);
-
-
-  // // Set up the color preview boxes
-  // setupColorPreviews();
-  //
-  // // Set up color mixer
-  // $('.wheel-color-picker').on('sliderup', function(event) {
-  //   setSliderRGBW($(event.target).parents('.led-input-container'));
-  // });
-  //
-  // // Set up LED input containers
-  // let containers = $('.led-input-container');
-  // containers.each(function (i) {
-  //   let rgbw = getWheelColor(containers[i]);
-  //   updateColorPreview(containers[i], rgbw);
-  //   // Set up direct RGBW input sliders
-  //   setupInputRows(containers[i]);
-  //   // Set up RGBW input submit
-  //   $('.color-input').on('change', function(event) {
-  //     setWheelRGBW('.led-input-container');
-  //   });
-  // });
-  //
-  // updateFavoriteThumbnails();
-  // setupLoadSelected();
 }
-
 
 document.onload = init();
